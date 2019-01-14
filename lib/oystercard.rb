2354a32d02796dc7d8c4280ee3025f1e7cbf776d
Vaith Schmitz <vaithschmitz@Vaiths-MacBook-Pro.local>
2019-01-14 17:03:25 +0000

    step7 use method to check amount validity

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -8,14 +8,18 @@ def initialize
 end
 
 def top_up(amount)
-	raise "Not A Valid Amount." if !amount.is_a?(Numeric) || amount.negative? || amount == 0
+	raise "Not A Valid Amount." if invalid_amount?(amount)
   raise "Exceeding maximum balance (#{MAX_BALANCE})." if (@balance + amount) > MAX_BALANCE
 	@balance += amount
 end
 
 def deduct(amount)
-	raise "Not A Valid Amount." if !amount.is_a?(Numeric) || amount.negative? || amount == 0
+	raise "Not A Valid Amount." if invalid_amount?(amount)
 	@balance -= amount
+end
+
+def invalid_amount?(amount)
+	!amount.is_a?(Numeric) || amount.negative? || amount == 0
 end
 
 end
