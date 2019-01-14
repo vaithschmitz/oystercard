1d8c1ea54ac2073dd96316501508eb4e1ba9abde
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-01-14 15:02:45 +0000

    Added balance property to oystercard

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,8 @@
-class Oystercard 
+class Oystercard
+attr_reader :balance
+
+def initialize
+  @balance = 1
+end
 
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -1,4 +1,9 @@
 require 'oystercard'
 
-describe Oystercard do 
+describe Oystercard do
+  context "@balance" do
+    it "checks if card has balance" do
+      expect(subject.instance_variable_get(:@balance)).to eq 1
+    end
+  end
 end
