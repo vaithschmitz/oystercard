6c386d9e5461461684341e63713bffaa6ece952a
Vaith Schmitz <vaithschmitz@Vaiths-MacBook-Pro.local>
2019-01-15 16:30:06 +0000

    step10

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -15,21 +15,13 @@ def top_up(amount)
 	@balance += amount
 end
 
-def deduct(amount)
-	raise "Not A Valid Amount." if invalid_amount?(amount)
-	@balance -= amount
-end
-
-def invalid_amount?(amount)
-	!amount.is_a?(Numeric) || amount.negative? || amount == 0
-end
-
 def touch_in
   raise "Balance Not High Enough For Current Journey." if @balance < MIN_FARE
   @card_in_use = true
 end
 
 def touch_out
+  deduct(MIN_FARE)
   @card_in_use = false
 end
 
@@ -37,4 +29,15 @@ def in_journey?
   @card_in_use
 end
 
+private
+  def deduct(amount)
+    raise "Not A Valid Amount." if invalid_amount?(amount)
+    @balance -= amount
+  end
+
+  def invalid_amount?(amount)
+    !amount.is_a?(Numeric) || amount.negative? || amount == 0
+  end
 end
+
+
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -13,26 +13,11 @@ describe Oystercard do
   		subject.top_up(2)
   		expect(subject.balance).to eq 5
   	end
-    it "throws error if invalid amount" do
-      expect{subject.top_up(-1)}.to raise_error("Not A Valid Amount.")
-      expect{subject.top_up("string")}.to raise_error("Not A Valid Amount.")
-    end
     it "throws error if topped up past max balance (£90)" do
       subject.instance_variable_set(:@balance,85)
       expect { subject.top_up(6) }.to raise_error("Exceeding maximum balance (#{Oystercard::MAX_BALANCE}).")
     end
   end
-
-  context "#deduct" do
-  	it "deducts amount from balance" do
-  		subject.instance_variable_set(:@balance, 5)
-  		subject.deduct(3)
-  		expect(subject.balance).to eq 2
-  	end
-  	it "throws error if invalid amount" do
-      expect{subject.deduct(-1)}.to raise_error("Not A Valid Amount.")
-      expect{subject.deduct("string")}.to raise_error("Not A Valid Amount.")
-  	end
 
   context "#touch_in" do
     it "throws error if balance is lower than minimum fare" do
@@ -46,16 +31,20 @@ describe Oystercard do
   end
 
   context "#touch_out" do
-    it " can touch out and end journey" do
+    it "can touch out and end journey" do
       subject.instance_variable_set(:@card_in_use, true)
       subject.touch_out
       expect(subject.in_journey?).to eq(false)
     end
-
+    it "deducts fare from balance upon touching out" do
+      subject.instance_variable_set(:@balance, 20) 
+      expect{subject.touch_out}.to change{subject.balance}.by(-Oystercard::MIN_FARE)
+    end
   end
+
   context "#in_journey" do
     it { is_expected.to respond_to(:in_journey?) }
-  end
+    end
   end
 
-end
+
